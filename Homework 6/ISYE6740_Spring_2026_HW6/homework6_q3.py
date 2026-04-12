da95aeb7272b1603e9d66a3cfd0ce31f131a3181
Greg <gregfsimioni@gmail.com>
2026-04-11 20:56:17 -0400

    finished q3

--- Homework 6/ISYE6740_Spring_2026_HW6/homework6_q3.py
+++ Homework 6/ISYE6740_Spring_2026_HW6/homework6_q3.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import OneClassSVM
+from sklearn.preprocessing import StandardScaler
 
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -20,7 +22,7 @@ y = df.iloc[:, -1].values.astype(int)
 X = df.iloc[:, :-1].values
 
 # train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=6740, shuffle=True, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=6740, shuffle=True, stratify=y)
 
 # ----> CART
 cart = DecisionTreeClassifier(
@@ -60,7 +62,7 @@ plt.tight_layout()
 
 save_path = os.path.join(script_dir, "results", "cart_spambase.png")
 plt.savefig(save_path)
-plt.show()
+# plt.show()
 
 # -----> Random Forest
 tree_grid = list(range(1, 201, 4))
@@ -92,7 +94,7 @@ plt.tight_layout()
 
 ranf_path = os.path.join(script_dir, "results", "ranf_cart_test_error.png")
 plt.savefig(ranf_path, bbox_inches="tight")
-plt.show()
+# plt.show()
 
 # -----> sensitivity exploration
 
@@ -138,9 +140,64 @@ plt.tight_layout()
 
 sens_path = os.path.join(script_dir, "results", "rf_oob_test.png")
 plt.savefig(sens_path)
-plt.show()
+# plt.show()
 
+# -----> 1 Class SVM
+# need to shuffle and split .75/.25
+X_train_ocsvm, X_test_ocsvm, y_train_ocsvm, y_test_ocsvm = train_test_split(X, y, test_size=0.25, random_state=6740, shuffle=True, stratify=y)
 
+# need to use only label 0 (non spam) for training
+# extract all non-spam emails from the training block
+X_train_ocsvm_nonspam = X_train_ocsvm[y_train_ocsvm == 0]
 
+# validation set for tuning
+X_train_ocsvm_spam = X_train_ocsvm[y_train_ocsvm == 1]
 
+# split training block (non spam one) for fit and validation
+# using 80/20 for this
+X_fit_nonspam, X_val_nonspam = train_test_split(X_train_ocsvm_nonspam, test_size=0.25, random_state=6740, shuffle=True)
 
+# need to create the validation set for tuning by combining the val non spam and the spam portion of the training block
+X_val_ocsvm = np.vstack((X_val_nonspam, X_train_ocsvm_spam))
+y_val_ocsvm = np.hstack((np.ones(X_val_nonspam.shape[0]), -np.ones(X_train_ocsvm_spam.shape[0])))
+# scale
+oscvm_scaler = StandardScaler()
+X_fit_nonspam_scaled = oscvm_scaler.fit_transform(X_fit_nonspam)
+X_val_ocsvm_scaled = oscvm_scaler.transform(X_val_ocsvm)
+
+# TUNING
+# init conds
+best_error = np.inf
+best_gamma = None
+best_nu = None
+# grid search parms
+gammas = [0.001, 0.01, 0.1, 1]
+nus = [0.01, 0.05, 0.1, 0.5]
+
+for nu in nus:
+    for gam in gammas:
+        ocsvm = OneClassSVM(kernel='rbf', gamma=gam, nu=nu)
+        ocsvm.fit(X_fit_nonspam_scaled)
+        # predict on val set
+        y_val_pred = ocsvm.predict(X_val_ocsvm_scaled)
+        # misclass error
+        val_error = np.mean(y_val_pred != y_val_ocsvm)
+        if val_error < best_error:
+            best_error = val_error
+            best_gamma = gam
+            best_nu = nu
+
+print(f"best gamma: {best_gamma}, best nu: {best_nu}, best val error: {best_error:.4f}")
+
+# need to use the best tuning params for the final fit
+X_train_ocsvm_nonspam_scaled = oscvm_scaler.fit_transform(X_train_ocsvm_nonspam)
+ocsvm_final = OneClassSVM(kernel='rbf', gamma=best_gamma, nu=best_nu)
+ocsvm_final.fit(X_train_ocsvm_nonspam_scaled)
+# predict on test set
+X_test_ocsvm_scaled = oscvm_scaler.transform(X_test_ocsvm)
+y_test_pred = ocsvm_final.predict(X_test_ocsvm_scaled)
+
+# need to remap because of -1 and 1 labels from before
+y_test_ocsvm_mapped = np.where(y_test_ocsvm == 0, 1, -1)
+ocsvm_test_error = np.mean(y_test_pred != y_test_ocsvm_mapped)
+print(f"one class svm test error: {ocsvm_test_error:.4f}")
